2c23198a06d9739b675bb91f3bb9b5c21ba87c9b
makitosima <makitosima@gmail.com>
2023-08-17 23:47:16 +0900

    bbbbb

--- main2.tf
+++ main2.tf
@@ -1,10 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
 }
 
 resource "aws_vpc" "test-vpc-aaaaa" {
